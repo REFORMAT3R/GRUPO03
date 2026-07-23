02aa232275ab8726072684849c7665e11c305d98
Haku021 <jgonzalesra@unsa.edu.pe>
2026-07-23 15:42:07 -0500

    Corriguiendo paciente

--- frontend/src/app/services/paciente.service.ts
+++ frontend/src/app/services/paciente.service.ts
@@ -4,19 +4,14 @@ import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Paciente } from '../models/paciente';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class PacienteService {
 
-
   private apiUrl = environment.apiUrl;
 
-
-  constructor(
-    private http: HttpClient
-  ) {}
+  constructor(private http: HttpClient) {}
 
   private pacienteEdit: Paciente | null = null;
 
@@ -29,42 +24,22 @@ export class PacienteService {
   }
 
   limpiarPacienteEditar() {
-      this.pacienteEdit = null;
-    }
+    this.pacienteEdit = null;
+  }
 
   getPacientes(): Observable<Paciente[]> {
-
-    return this.http.get<Paciente[]>(this.apiUrl);
-
+    return this.http.get<Paciente[]>(`${this.apiUrl}/pacientes/`);
   }
-
 
   crearPaciente(paciente: Paciente): Observable<Paciente> {
-
-    return this.http.post<Paciente>(
-      this.apiUrl,
-      paciente
-    );
-
+    return this.http.post<Paciente>(`${this.apiUrl}/pacientes/`, paciente);
   }
 
-
-  actualizarPaciente(id:number, paciente:Paciente):Observable<Paciente>{
-
-    return this.http.put<Paciente>(
-      `${this.apiUrl}${id}/`,
-      paciente
-    );
-
+  actualizarPaciente(id: number, paciente: Paciente): Observable<Paciente> {
+    return this.http.put<Paciente>(`${this.apiUrl}/pacientes/${id}/`, paciente);
   }
 
-
-  eliminarPaciente(id:number):Observable<any>{
-
-    return this.http.delete(
-      `${this.apiUrl}${id}/`
-    );
-
+  eliminarPaciente(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/pacientes/${id}/`);
   }
-
 }
